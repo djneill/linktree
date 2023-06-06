47a0345201a6582d888b2ae7f99316f911b883df
DJ Neill <dj.neill856@gmail.com>
2023-06-06 00:43:25 -0500

    add function to copy link to clipboard

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
-const shareButton = document.getElementById('shareButton');
-const shareOptions = {
-    title: 'Share',
-    text: 'Check out my Linktree!',
-    url: 'https://your-linktree-url.com'
-};
+function copyLink() {
+    const link = 'https://linktreedjneill.netlify.app/'
 
-shareButton.addEventListener('click', () => {
-    if (navigator.share) {
-        navigator.share(shareOptions)
-            .then(() => console.log('Successful share'))
-            .catch((error) => console.log('Error sharing:', error));
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(link)
+            .then(() => alert('Link copied to clipboard'))
+            .catch((error) => alert('Error copying link:', error));
     } else {
-        console.log('Web Share API not supported');
+        console.log('Clipboard API not supported');
     }
-});
+}
