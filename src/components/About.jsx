ecca3d3290f4f08ea3c0d7e5c44521fba9f4f51c
DJ Neill <dj.neill856@gmail.com>
2023-09-11 14:15:05 -0500

    update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ const About = () => {
     return (
         <div className="about-container">
             <h1 className="about font-Inter">About</h1>
-            <p className="description">Creative, detail-oriented, software engineer with a deep interest in full-stack engineering. Proven track record of creating and implementing successful web applications. Looking to bring my skills to a tech company with global reach.</p>
+            <p className="description">Experienced full-stack software engineer with a passion for creating innovative web applications. Proven ability to translate business requirements into technical solutions.</p>
         </div>
     )
 }
